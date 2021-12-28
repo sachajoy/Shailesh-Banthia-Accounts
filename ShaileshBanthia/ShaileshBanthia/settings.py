5e3b3e3ff1807806805a2c254ae42d70ab4937b1
sachajoy <me.arihant.banthia@gmail.com>
2021-12-28 18:54:43 +0530

    Added the STATIC_ROOT for apps in the django and STATICFILES_DIRS for the whole application

--- ShaileshBanthia/ShaileshBanthia/settings.py
+++ ShaileshBanthia/ShaileshBanthia/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assests')
+STATICFILES_DIRS = [
+    BASE_DIR / 'assests',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
