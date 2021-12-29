db78eafac5ae0e5205f755005cd6853009bd305b
sachajoy <me.arihant.banthia@gmail.com>
2021-12-29 21:46:03 +0530

    Implemented the login of user in the applicaitons 	- Added the LOGIN_REQUIRED_URL in setting.py 	- Added the django.contrib.auth.urls in urls.py 	- Added the Login Template in Registration

--- ShaileshBanthia/ShaileshBanthia/urls.py
+++ ShaileshBanthia/ShaileshBanthia/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib import auth
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('', include('authentication.urls', 'authenticaiton')),
+    path('accounts/login/', auth_view.LoginView.as_view()),
     path('admin/', admin.site.urls),
 ]
