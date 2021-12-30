e96fcab56304509d0a397859e7711ef28aba22d7
sachajoy <me.arihant.banthia@gmail.com>
2021-12-30 14:02:08 +0530

    Create the template.html which is now base with navbar.html consisting of navbar and add it to whole project

--- ShaileshBanthia/authentication/views.py
+++ ShaileshBanthia/authentication/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required()
 def index(request):
-    if request.user.is_authenticated:
-        return render(
-            request,
-            'authentication/index.html',
-            {
-                'name': 'User is loged in'
-            }
-        )
-    context = {
-        'user': request.user
-    }
     return render(
-        request, 
+        request,
         'authentication/index.html',
-        context
+        {
+            'dashboard': 1,
+        }
     )
